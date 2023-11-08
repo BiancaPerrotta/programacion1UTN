72d4ff41f441be8cc4f5f67c1caed03cafe24a1d
BiancaPerrotta <145625057+BiancaPerrotta@users.noreply.github.com>
2023-11-07 21:48:11 -0300

    Add files via upload

---
+++ ParcialProg2-Perrotta/test_funciones2.py
@@ -0,0 +1,25 @@
+
+import funciones2
+
+
+def test_is_mutant_true():
+    adn_list = [
+        "AAAAAA",
+        "TTTTTT",
+        "CCCCCC",
+        "GGGGGG",
+        "AAAAAA",
+        "TTTTTT"
+    ]
+    assert funciones2.is_mutant(adn_list) == True
+
+def test_is_mutant_false():
+    adn_list = [
+        "ACTATG",
+        "ATCTTT",
+        "CTCACG",
+        "GGTGAG",
+        "ATACAA",
+        "GAGTTT"
+    ]
+    assert funciones2.is_mutant(adn_list) == False
